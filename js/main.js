b5c4e617fee39285588f6dcc857e6c5473c46362
damianrequejo <drequejo@gmail.com>
2022-06-28 13:07:14 -0300

    Segunda Entrega -v2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-let pfObj = [];
+let plazosFijos = [];
+const listaPf = document.querySelector('#lista-plazosfijos');
 
 // Simular PF 
 btnSimular.addEventListener("click", (e) => {
@@ -21,8 +22,10 @@ btnSimular.addEventListener("click", (e) => {
                     id: Date.now(),
                     detalle: depositoCapital,duracion,tna
                 }
-                console.log(pfObj);
-                localStorage.setItem("id", JSON.stringify(pfObj));
+
+                plazosFijos = [...plazosFijos, pfObj]
+                console.log(plazosFijos);
+                localStorage.setItem("id", JSON.stringify(plazosFijos));
             } 
             else {
                 document.getElementById("clac_error").innerHTML = 'Ingrese valor mayor a $1000.';
@@ -30,10 +33,37 @@ btnSimular.addEventListener("click", (e) => {
     }else{
             document.getElementById("clac_error").innerHTML = 'Ingrese un plazo mayor a 30.';
     }
+   
+    limpiarHTML();
+    
+    if(plazosFijos.length > 0) {
+        plazosFijos.forEach( pfObj =>  {
+            
+            // Crear elemento y añadirle el contenido a la lista
+            const li = document.createElement('li');
 
+            // Añade el id del plazo fijo
+            li.innerText = pfObj.id;
+            
+            // añade un atributo único...
+            li.dataset.plazosfijosId = pfObj.id;
+
+            // añade el plazo fijo a la lista
+            listaPf.appendChild(li);
+
+        })
+    }
 });
 
 // Borra Storage
 btnBorrarStorage.addEventListener("click", (e) => {
     localStorage.clear();
+    plazosFijos = [];
+    limpiarHTML();
 });
+
+function limpiarHTML() {
+    while(listaPf.firstChild) {
+         listaPf.removeChild(listaPf.firstChild);
+    }
+}
